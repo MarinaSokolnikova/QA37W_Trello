b0f35a5f82d165b33ff4552ec8bbe28affbb3bb6
MarinaSokolnikova <sokolnikovam92@gmail.com>
2023-03-23 17:49:13 +0200

    login

--- src/test/java/tests/Login.java
+++ src/test/java/tests/Login.java
@@ -1,10 +1,20 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Login {
+public class Login  extends TestBase{
     @Test
     public void login1(){
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillLoginForm("sokolnikovam92@gmail.com", "FF22Kseriesqq!");
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+        Assert.assertTrue(app.getUser().isLogged());
+
+
 
     }
 }
