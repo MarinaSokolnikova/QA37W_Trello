bcc2c22ec4a56486bb87ad44572e090e27f2f82f
MarinaSokolnikova <sokolnikovam92@gmail.com>
2023-03-26 13:59:25 +0300

    boardCretion

--- src/main/java/manage/ApplicationManager.java
+++ src/main/java/manage/ApplicationManager.java
@@ -1,9 +1,14 @@
 package manage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 
+import javax.swing.*;
 import java.time.Duration;
 
 public class ApplicationManager {
@@ -11,6 +16,7 @@ public class ApplicationManager {
     WebDriver wd;
 
     UserHelper user;
+    BoardHelper board;
 
     public void init(){
         ChromeOptions options = new ChromeOptions();
@@ -21,14 +27,23 @@ public class ApplicationManager {
         wd.navigate().to("https://trello.com/");
 
         user = new UserHelper(wd);
+        board = new BoardHelper(wd);
+        user.login("sokolnikovam92@gmail.com", "FF22Kseriesqq!");
+
     }
 
     public UserHelper getUser() {
         return user;
     }
 
+    public BoardHelper getBoard() {
+        return board;
+    }
+
     public void quit(){
         wd.close();
         wd.quit();
     }
+
+
 }
--- src/main/java/manage/UserHelper.java
+++ src/main/java/manage/UserHelper.java
@@ -26,4 +26,19 @@ public class UserHelper extends HelperBase{
     public boolean isLogged() {
         return wd.findElements(By.cssSelector("[data-testid='header-member-menu-button']")).size()>0;
     }
+
+    public void logout() {
+        click(By.xpath("//button[@data-testid='header-member-menu-button']"));
+        click(By.xpath("//button[@data-testid='account-menu-logout']"));
+        click(By.xpath("//button[@data-testid='logout-button']"));
+    }
+
+    public void login(String email, String password) {
+        click(By.cssSelector("[href='/login']"));
+        type(By.cssSelector("#user"), email);
+        click(By.cssSelector("#login"));
+        pause(2000);
+        type(By.cssSelector("#password"), password);
+        click(By.cssSelector("#login-submit"));
+    }
 }
